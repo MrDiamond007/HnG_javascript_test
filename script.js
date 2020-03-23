4f4cbf1befd9a1839fc7ef4335cb0dc844ed1a3c
ganiyu babatunde <ganiyu babatunde>
2020-03-24 00:12:03 +0100

    On branch master

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var name = "Ganiyu Babatunde";
+
+var courses = ["front-end", "Design","Coding" ];
+
+console.log("i am  " + name);
+console.log("am into  " + courses); 
+
+var i;
+for (i = 0; i < 200; i++)
+	if ((i % 3) == 0)
+		console.log(i);
